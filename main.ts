ce9d317e45b011ecb4e14f65adcaa3544cebac4f
n0thlng8 <129434214+n0thlng8@users.noreply.github.com>
2023-04-17 14:26:55 -0400

    bras-bras

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function vitmoteur3 (angleinitial: number, anglefinal: number) {
+    for (let index = 0; index <= Math.abs(angleinitial - anglefinal); index++) {
+        if (anglefinal > angleinitial) {
+            pins.servoWritePin(AnalogPin.P2, angleinitial + index)
+        } else if (anglefinal < angleinitial) {
+            pins.servoWritePin(AnalogPin.P2, angleinitial - index)
+        }
+        basic.pause(10)
+    }
+    anglemoteur3 = anglefinal
+}
 function Moteur3 () {
     basic.showLeds(`
         . # # . .
@@ -9,8 +20,10 @@ function Moteur3 () {
     basic.pause(2000)
     if (input.buttonIsPressed(Button.A)) {
         pins.servoWritePin(AnalogPin.P2, 180)
+        vitmoteur3(anglemoteur3, 180)
     } else if (input.buttonIsPressed(Button.B)) {
-        pins.servoWritePin(AnalogPin.P2, 0)
+        pins.servoWritePin(AnalogPin.P2, 35)
+        vitmoteur3(anglemoteur3, 35)
     }
 }
 function Moteur1 () {
@@ -23,9 +36,11 @@ function Moteur1 () {
         `)
     basic.pause(2000)
     if (input.buttonIsPressed(Button.A)) {
-        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P0, 135)
+        vitmoteur1(anglemoteur1, 135)
     } else if (input.buttonIsPressed(Button.B)) {
-        pins.servoWritePin(AnalogPin.P0, 0)
+        pins.servoWritePin(AnalogPin.P0, 40)
+        vitmoteur1(anglemoteur1, 40)
     }
 }
 function Moteur2 () {
@@ -37,11 +52,24 @@ function Moteur2 () {
         # # # # .
         `)
     basic.pause(2000)
-    if (input.buttonIsPressed(Button.B)) {
-        pins.servoWritePin(AnalogPin.P1, 180)
-    } else if (input.buttonIsPressed(Button.A)) {
-        pins.servoWritePin(AnalogPin.P1, 0)
+    if (input.buttonIsPressed(Button.A)) {
+        pins.servoWritePin(AnalogPin.P1, 135)
+        vitmoteur2(anglemoteur2, 135)
+    } else if (input.buttonIsPressed(Button.B)) {
+        pins.servoWritePin(AnalogPin.P1, 40)
+        vitmoteur2(anglemoteur2, 40)
     }
+}
+function vitmoteur2 (angleinitial: number, anglefinal: number) {
+    for (let index = 0; index <= Math.abs(angleinitial - anglefinal); index++) {
+        if (anglefinal > angleinitial) {
+            pins.servoWritePin(AnalogPin.P1, angleinitial + index)
+        } else if (anglefinal < angleinitial) {
+            pins.servoWritePin(AnalogPin.P1, angleinitial - index)
+        }
+        basic.pause(10)
+    }
+    anglemoteur2 = anglefinal
 }
 function vitmoteur1 (angleinitial: number, anglefinal: number) {
     for (let index = 0; index <= Math.abs(angleinitial - anglefinal); index++) {
@@ -54,17 +82,18 @@ function vitmoteur1 (angleinitial: number, anglefinal: number) {
     }
     anglemoteur1 = anglefinal
 }
+let anglemoteur2 = 0
 let anglemoteur1 = 0
+let anglemoteur3 = 0
 basic.showLeds(`
     # # . # #
     # # . # #
-    # . . . .
+    . . . . .
     # # . # #
     # # . # #
     `)
 basic.pause(1000)
 pins.servoWritePin(AnalogPin.P0, 90)
-anglemoteur1 = 90
 pins.servoWritePin(AnalogPin.P1, 90)
 pins.servoWritePin(AnalogPin.P2, 90)
 basic.showLeds(`
@@ -84,7 +113,6 @@ basic.forever(function () {
         `)
     if (input.buttonIsPressed(Button.A)) {
         Moteur1()
-        vitmoteur1(anglemoteur1, 180)
     } else if (input.buttonIsPressed(Button.B)) {
         Moteur2()
     } else if (input.isGesture(Gesture.Shake)) {
